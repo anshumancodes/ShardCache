ce1a1f68f7a3bfaeaec8652b3a1a85a4fae2be76
anshumancodes <anshumanprof01@gmail.com>
2026-09-21 00:38:53 +0530

    feat(cache): implement basic cache functionality

--- cache/cache.go
+++ cache/cache.go
@@ -1,9 +1,39 @@
 package cache
 
-import (
-	"fmt"
-)
+type Cache struct {
+	shards []Shard // cache is made of shards (slice of shards)
+}
 
-func Cache() {
-	fmt.Println("cache package")
+// cache constructor
+func New(numShards int) *Cache {
+	shards := make([]Shard, numShards) // returns a slice of shards
+	for i := range shards {
+		shards[i].data = make(map[string]string)
+	}
+	return &Cache{shards: shards}
+}
+
+// passing key and value to set function
+// then the key is used to get the shard on which the value is to be set , using the GetShard function
+// once the shard is obtained , we lock it and set the value
+// then unlock it
+func (c *Cache) Set(key, value string) {
+	shard := c.GetShard(key)
+	shard.mu.Lock()
+	shard.data[key] = value
+	shard.mu.Unlock()
+}
+func (c *Cache) Get(key string) (string, bool) {
+
+	shard := c.GetShard(key)
+	shard.mu.RUnlock()
+	defer shard.mu.RUnlock()
+
+	value, ok := shard.data[key]
+
+	if !ok {
+		return "", false
+	}
+
+	return value, true
 }
