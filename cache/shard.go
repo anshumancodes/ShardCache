7250c9fc3048f67fa84077a258d583fda61214d1
anshumancodes <anshumanprof01@gmail.com>
2026-09-21 00:38:30 +0530

    feat(cache): add GetShard method

--- cache/shard.go
+++ cache/shard.go
@@ -1,7 +1,22 @@
 package cache
 
-import "fmt"
+import (
+	"hash/fnv"
+	"sync"
+)
 
-func Shard() {
-	fmt.Println("shard package")
+type Shard struct {
+	data map[string]string // shard data
+	mu   sync.RWMutex      // lock for the shard
+}
+
+// pass a cache key to get the shard
+// it converts the key to a hash and uses it to determine the shard index
+// now once shard index is determined, it returns the shard from Cache struct
+func (c *Cache) GetShard(key string) *Shard {
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	index := h.Sum32() % uint32(len(c.shards))
+
+	return &c.shards[index]
 }
