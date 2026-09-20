0c9783139e2670333a40bd7413ffac87a6b7751a
anshumancodes <anshumanprof01@gmail.com>
2026-09-21 00:59:01 +0530

    feat(cache): implement eviction logic

--- cache/eviction.go
+++ cache/eviction.go
@@ -1,6 +1,33 @@
 package cache
-import "fmt"
 
-func Evict() {
-	fmt.Println("eviction package")
+import (
+	"time"
+)
+
+type Eviction struct {
+	key          string
+	creationTime time.Time
+}
+
+const TTL = 10 * time.Second
+
+func (e *Eviction) IsExpired() bool {
+	return time.Since(e.creationTime) >= TTL
+}
+
+func NewEviction(key string) *Eviction {
+	return &Eviction{
+		key:          key,
+		creationTime: time.Now(),
+	}
+}
+
+func HandleEviction(shard *Shard, evictions []*Eviction) {
+	for _, eviction := range evictions {
+		if eviction.IsExpired() {
+			// delete from the shard's data map only if the eviction is expired
+			delete(shard.data, eviction.key)
+		}
+
+	}
 }
