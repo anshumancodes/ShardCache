9fe5ba2c24e4c3b80adda69b27f0939026214c95
anshumancodes <anshumanprof01@gmail.com>
2026-09-21 14:02:55 +0530

    feat(http): integrate ShardCache HTTP handler

--- main.go
+++ main.go
@@ -1,26 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/anshumancodes/ShardCache/cache"
+	cachehttp "github.com/anshumancodes/ShardCache/http"
 	"github.com/joho/godotenv"
 )
-
-type Response struct {
-	Data    any    `json:"data"`
-	Message string `json:"message"`
-}
-
-func HelloFrom(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	response := &Response{Message: "http server is running sucessfully!"}
-	json.NewEncoder(w).Encode(response)
-}
 
 func main() {
 	// loading from .env file
@@ -29,12 +17,18 @@ func main() {
 		fmt.Println("Error loading .env file")
 		return
 	}
+	c := cache.New(6)
+
+	handler := cachehttp.NewHandler(c)
 
 	// initialize the server
 	mux := http.NewServeMux()
 
 	// routes
-	mux.HandleFunc("/", HelloFrom)
+	mux.HandleFunc("GET /", cachehttp.HelloFrom)
+	// cache routes
+	mux.HandleFunc("GET /cache/{key}", handler.Get)
+	mux.HandleFunc("POST /cache/{key}", handler.Set)
 
 	// port and server running configuration
 	PORT := os.Getenv("PORT")
@@ -43,7 +37,6 @@ func main() {
 		PORT = "8080"
 	}
 
-	cache.Evict()
 	fmt.Printf("server is running on %s\n", PORT)
 	serverError := http.ListenAndServe(":"+PORT, mux)
 	if serverError != nil {
